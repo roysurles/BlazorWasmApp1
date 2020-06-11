f1541c22f51b0db683a16ac2fc8c08dc053ea384
Roy Surles <surlesr@earthlink.net>
2020-06-10 21:02:17 -0500

    Add additional way to get extra claims.

--- BlazorWasmApp1/Client/Shared/CustomUserFactory.cs
+++ BlazorWasmApp1/Client/Shared/CustomUserFactory.cs
@@ -1,7 +1,11 @@
 ﻿using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Threading.Tasks;
+
+using BlazorWasmApp1.Shared.Models;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
@@ -10,8 +14,11 @@ namespace BlazorWasmApp1.Client.Shared
 {
     public class CustomUserFactory : AccountClaimsPrincipalFactory<RemoteUserAccount>
     {
-        public CustomUserFactory(IAccessTokenProviderAccessor accessor) : base(accessor)
+        private readonly IHttpClientFactory _clientFactory;
+
+        public CustomUserFactory(IAccessTokenProviderAccessor accessor, IHttpClientFactory clientFactory) : base(accessor)
         {
+            _clientFactory = clientFactory;
         }
 
         public async override ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account, RemoteAuthenticationUserOptions options)
@@ -49,6 +56,12 @@ namespace BlazorWasmApp1.Client.Shared
                 }
 
                 identity.AddClaim(new Claim("MyCustomClaim", "MyCustomClaim.Value"));
+
+                // this is one way to get additional claims...
+                var httpClient = _clientFactory.CreateClient("BlazorWasmApp1.ServerAPI");
+                var forecasts = await httpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+                foreach (var forecast in forecasts)
+                    identity.AddClaim(new Claim(forecast.Summary, forecast.Date.ToString()));
             }
 
             return user;
