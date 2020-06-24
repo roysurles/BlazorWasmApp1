7a2456a2f68ed0f027172192b53934f1d2a5bd8e
Roy Surles <surlesr@earthlink.net>
2020-06-24 14:45:42 -0500

    Test organization by feature.

---
+++ BlazorWasmApp1.Core.Api/Features/FeatureOne/FeatureOneController.cs
@@ -0,0 +1,21 @@
+﻿
+using BlazorWasmApp1.Shared.Controllers;
+using BlazorWasmApp1.Shared.Models.Common.ApiResult;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmApp1.Core.Api.Features.FeatureOne
+{
+    [AllowAnonymous]
+    [ApiVersion("1")]
+    [ControllerName("FeatureOne")]
+    [Route("api/v{version:apiVersion}/FeaureOne")]
+    public class FeatureOneController : BaseApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult<IApiResultModel<string>> Get() =>
+            CreateActionResult("Hello");
+    }
+}
